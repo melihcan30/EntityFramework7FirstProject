ecde97f5810de32e3d4488327dce3c416b39c33b
Melihcan Serbest <canmelih.free3@gmail.com>
2023-05-04 15:45:42 +0300

    The ConnectionString

--- EntityFramework7FirstProject/Data/DataContext.cs
+++ EntityFramework7FirstProject/Data/DataContext.cs
@@ -4,5 +4,17 @@ namespace EntityFramework7FirstProject.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=dotnet7testdb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<WeatherForecast> Forecasts { get; set; }
     }
 }
